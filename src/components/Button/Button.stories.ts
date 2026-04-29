7733fcccb4424ad1245362f0777b1f947ceac973
steffiting <yskim98@yahoo.com>
2026-04-29 11:18:48 +0800

    [PROJ-109] Button

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { fn } from 'storybook/test'
+import { Button } from './Button'
+import changelog from './Button.changelog.md?raw'
+import pkg from '../../../package.json'
+
+const meta = {
+    title: 'Components/Button',
+    component: Button,
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component: `
+### Current Version
+**v${pkg.version}**
+
+${changelog}
+        `,
+            }
+        }
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: { onClick: fn() },
+} satisfies Meta<typeof Button>
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+    args: {
+        id: 'primary-button',
+        testId: 'primary-button',
+        primary: true,
+        label: 'Button',
+    },
+}
+
+export const Secondary: Story = {
+    args: {
+        id: 'secondary-button',
+        testId: 'secondary-button',
+        primary: false,
+        label: 'Button',
+    },
+}
