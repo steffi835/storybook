7ae984f948e4a94fb8cdf075a1ea9f1e357e5bd5
steffiting <yskim98@yahoo.com>
2026-04-29 11:09:10 +0800

    [PROJ-107] Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,42 @@
+import './button.css'
+
+export interface ButtonProps {
+    id: string
+    testId: string
+    
+    label: string
+    onClick: () => void
+    
+    backgroundColor?: string
+    disabled?: boolean
+    primary?: boolean
+    size?: 'small' | 'medium' | 'large'
+}
+
+export const Button = (props: ButtonProps) => {
+    const {
+        id,
+        testId,
+        label,
+        onClick,
+        backgroundColor,
+        disabled = false,
+        primary = true,
+        size = 'medium'
+    } = props
+    const mode = primary ? 'button-primary' : 'button-secondary'
+
+    return (
+        <button
+            id={id}
+            data-testid={testId}
+            className={['button', `button-${size}`, mode].join(' ')}
+            disabled={disabled}
+            style={{ backgroundColor }}
+            type='button'
+            onClick={onClick}
+        >
+            {label}
+        </button>
+    )
+}
