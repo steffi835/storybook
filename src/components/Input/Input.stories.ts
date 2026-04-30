6de75369dcf3999d7af222c0bf0251543a6b1849
steffiting <yskim98@yahoo.com>
2026-05-01 00:15:59 +0800

    [PROJ-117] Input

---
+++ src/components/Input/Input.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { fn } from 'storybook/test'
+import { Input } from './Input'
+import changelog from './Input.changelog.md?raw'
+import pkg from '../../../package.json'
+
+const meta = {
+    title: 'Components/Input',
+    component: Input,
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component: `
+### Current Version
+**v${pkg.version}**
+
+${changelog}
+        `,
+            }
+        }
+    },
+    tags: ['autodocs'],
+    argTypes: {},
+    args: { onChange: fn() },
+} satisfies Meta<typeof Input>
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+    args: {
+        id: 'input-primary',
+        testId: 'input-primary'
+    },
+}
+
+export const Secondary: Story = {
+    args: {
+        id: 'input-secondary',
+        testId: 'input-secondary',
+        primary: false
+    },
+}
