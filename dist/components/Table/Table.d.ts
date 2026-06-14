cf8c03a4b231cf90b45d18266008a90c9a9a0204
steffiting <yskim98@yahoo.com>
2026-06-14 19:41:23 +0800

    update version 1.1.2

---
+++ dist/components/Table/Table.d.ts
@@ -0,0 +1,14 @@
+export interface TableColumn<T> {
+    label: string;
+    key?: keyof T;
+    render?: (row: T) => React.ReactNode;
+}
+export interface TableProps<T> {
+    id: string;
+    columns: TableColumn<T>[];
+    data: T[];
+    hover?: boolean;
+    primary?: boolean;
+    striped?: boolean;
+}
+export declare const Table: <T extends Record<string, unknown>>(props: TableProps<T>) => import("react").JSX.Element;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,4 @@ export * from './components/Button/Button';
 export * from './components/Dropdown/Dropdown';
 export * from './components/Input/Input';
 export * from './components/Label/Label';
+export * from './components/Table/Table';
