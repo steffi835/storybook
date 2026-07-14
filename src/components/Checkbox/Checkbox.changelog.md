58a8bc8e5171a6f835d051ea690d9c1ab454fab0
steffiting <yskim98@yahoo.com>
2026-07-14 16:31:00 +0800

    [PROJ-134] Checkbox

---
+++ src/components/Checkbox/Checkbox.changelog.md
@@ -0,0 +1,3 @@
+### Changelog
+
+- **v1.0.0** - 14 Jul 2026 - Create component
---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,67 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { useState } from 'react'
+import { Checkbox } from './Checkbox'
+import changelog from './Checkbox.changelog.md?raw'
+import pkg from '../../../package.json'
+
+const meta = {
+    title: 'Components/Checkbox',
+    component: Checkbox,
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component: `
+### Current Version
+**v${pkg.version}**
+
+${changelog}
+        `,
+            }
+        }
+    },
+    tags: ['autodocs']
+} satisfies Meta<typeof Checkbox>
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    render: () => {
+        const [checked, setChecked] = useState(false)
+
+        return (
+            <Checkbox
+                id='checkbox'
+                label="Checkbox"
+                checked={checked}
+                onChange={setChecked}
+            />
+        )
+    },
+    args: {
+        id: 'checkbox',
+        label: 'Checkbox',
+        checked: false,
+        onChange: () => {}
+    }
+}
+
+export const Checked: Story = {
+    args: {
+        id: 'checkbox',
+        label: 'Checkbox',
+        checked: true,
+        onChange: () => {}
+    }
+}
+
+export const Disabled: Story = {
+    args: {
+        id: 'checkbox',
+        label: 'Checkbox',
+        checked: false,
+        disabled: true,
+        onChange: () => {}
+    }
+}
